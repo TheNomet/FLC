48999145eff22d3b08047ea79a4837e1b40ca230
TheNomet <fiore_matteo@hotmail.it>
2016-07-18 21:54:23 +0200

    personalized output

--- regexp/do.sh
+++ regexp/do.sh
@@ -5,19 +5,20 @@
 # parser.cup
 # Main.java
 # regexp.c
+# Makefile
 # a link to this script (or this script)
 
 # pass the INPUT file to be elaborated as first parameter of this script
 
 touch input.txt
-if [ $# -lt 2 ]; then
-	echo "Syntax: ./do.sh <from> <to> [<1 -from odd numbers> <0 -for even numbers>]?"
+if [ $# -lt 3 ]; then
+	echo "Syntax: ./do.sh <from> <to> <varname> [<1 -from odd numbers> <0 -for even numbers>]? "
 	exit
-elif [ $# -eq 2 ]; then #all the numbers
+elif [ $# -eq 3 ]; then #all the numbers
 	NUM=2
 	echo ALL > input.txt
-elif [ $# -eq 3 ]; then #only odd or even
-	if [ $3 -eq 0 ]; then #odd
+elif [ $# -eq 4 ]; then #only odd or even
+	if [ $4 -eq 0 ]; then #odd
 		NUM=0;
 		echo EVEN > input.txt
 	else #even
@@ -32,4 +33,16 @@ echo END >> input.txt
 
 #cat input.txt
 make 1> /dev/null 2>/dev/null
-java Main input.txt 
+final="$(java Main input.txt)" 
+
+IFS='='
+flag=0
+for word in $final 
+	do if [ $flag -eq 0 ]; then
+			flag=1;
+			echo -n "$3 = "
+		else
+			echo -n "$word"
+		fi
+done
+echo ""
